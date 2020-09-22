4584dcd29a1e973ce710a61eae16359ea9a12f5d
Gary Burner <grburner@gmail.com>
2020-09-22 14:33:39 -0400

    finished api routes

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,53 @@ const router = require('express').Router();
 const workout = require('../models/workout');
 
 router.get('/api/workouts', (req, res) => {
-  res.send('<h1>letsgo</h1>');
-  // workout.find()
-  // .then(data => {
-  //   res.json(data);
-  // });
+  workout.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    });
 });
 
-// router.put('/api/workouts/:id', (req, res) => {
-
-// });
+router.put('/api/workouts/:id', (req, res) => {
+  let params = req.params;
+  let body = req.body;
+  workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: { exercises: body }
+    },
+    {
+      new: true,
+    }
+  )
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
 
 router.post('/api/workouts', (req, res) => {
   workout.create({})
     .then(data => {
       res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
     });
 });
 
-// router.get('/api/workouts/range', (req, res) => {
-
-// });
+router.get('/api/workouts/range', (req, res) => {
+  workout.find()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(error => {
+      res.json(error);
+    });
+});
 
 module.exports = router;
