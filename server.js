885d3b07ab2650a391332cd5babe10fffa26ef34
Gary Burner <grburner@gmail.com>
2020-09-22 09:56:47 -0400

    adding eslint configs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
